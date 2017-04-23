2d5c34c082c64acabbd1a8c1ae7091bd16237776
linjian <linjian128@gmail.com>
2017-04-23 00:48:56 +0000

    modify folder

---
+++ raspi/raspi_assistant/handler.py
@@ -0,0 +1,252 @@
+# coding: utf-8
+import logging
+import traceback
+import datetime
+import requests
+from tempfile import NamedTemporaryFile
+
+import jieba
+from voicetools import BaiduVoice, TuringRobot
+
+from .settings import BasicConfig as BC, BaiduAPIConfig as BAC
+from .utils import (
+    AudioHandler, Keyword, cache, CacheHandler, timestamp, BaiduAPIClient,
+    generate_response, convert_to_wav)
+
+logger = logging.getLogger()
+
+FUNC_MAP = {
+    Keyword(['备忘录', ]).value: 'memo_today',
+    Keyword(['提醒', ]).value: 'memo_today',
+    Keyword(['备忘录', '今天']).value: 'memo_today',
+    Keyword(['提醒', '今天']).value: 'memo_today',
+    Keyword(['备忘录', '明天']).value: 'memo_tomo',
+    Keyword(['提醒', '明天']).value: 'memo_tomo',
+    Keyword(['备忘录', '播放']).value: 'play_memo_today',
+    Keyword(['提醒', '播放']).value: 'play_memo_today',
+    Keyword(['备忘录', '播放', '明天']).value: 'play_memo_tomo',
+    Keyword(['提醒', '播放', '明天']).value: 'play_memo_tomo',
+    Keyword(['备忘录', '删除']).value: 'del_all_memo',
+    Keyword(['提醒', '删除']).value: 'del_all_memo',
+    Keyword(['备忘录', '删除', '最后']).value: 'del_last_memo',
+    Keyword(['提醒', '删除', '最后']).value: 'del_last_memo',
+    Keyword(['备忘录', '删除', '第一条']).value: 'del_first_memo',
+    Keyword(['提醒', '删除', '第一条']).value: 'del_first_memo',
+    Keyword(['明天', '天气']).value: 'weather_tomo',
+    Keyword(['今天', '天气']).value: 'weather_today'
+}
+
+
+class BaseHandler(object):
+
+    def __init__(self, baidu_token=None):
+        if not baidu_token:
+            try:
+                token = BaiduVoice.get_baidu_token(BC.VOICE_API_KEY, BC.VOICE_SECRET)
+                self.token = token['access_token']
+            except Exception, e:
+                logger.warn('======Get baidu voice token failed, %s', traceback.format_exc())
+                raise e
+        else:
+            self.token = baidu_token
+        self.bv = BaiduVoice(self.token)
+        self.audio_handler = AudioHandler()
+
+    def __repr__(self):
+        return '<BaseHandler>'
+
+    def receive(self, sec=4):
+        self.feedback(generate_response())
+        self.audio_handler.arecord(sec)
+        try:
+            return self.bv.asr('record.wav')
+        except Exception, e:
+            logger.warn('======Baidu ASR failed, %s', traceback.format_exc())
+
+    def process(self, results):
+        seg_list = list(jieba.cut(results[0], cut_all=True))
+        # command = Keyword(list(set(seg_list) & BC.KEYWORDS))
+        command = Keyword(list(set((x.encode('utf-8') for x in seg_list)) & BC.KEYWORDS))
+        logger.info('=======Recognition result: %s', command.value)
+        return FUNC_MAP.get(command.value, 'default'), results[0]
+
+    def execute(self, func_name, result):
+        func = getattr(ActionHandler, func_name)
+        return func(self, result)
+
+    @cache
+    def feedback(self, content=None):
+        if content:
+            try:
+                data = NamedTemporaryFile()
+                data.write(self.bv.tts(content))
+                data.seek(0)
+                convert_to_wav(data)
+                data.close()
+            except Exception, e:
+                logger.warn('======Baidu TTS failed, %s', traceback.format_exc())
+            else:
+                self.audio_handler.aplay()
+
+    def worker(self):
+        try:
+            results = self.receive()
+            func, result = self.process(results)
+	    print "func = ===== %s"%func
+	    print "ressult = ===== %s"%result
+	    print "ressult type = ===== %s"%type(result).__name__
+	    print "kaideng = ===== %s"%type('开灯，').__name__
+	   # print "kaideng = ===== %s"%type(u('开灯，')).__name__
+            content = self.execute(func, result)
+	    if u'开灯' in result:
+	    	print "------------"
+		self.feedback('好的')
+		requests.get('http://192.168.6.107/?pin=ON1')
+		return
+	    elif u'关灯' in result:
+                print "------------"
+                self.feedback('好的')
+                requests.get('http://192.168.6.107/?pin=OFF1')
+                return
+
+            self.feedback(content)
+	
+        except Exception, e:
+            self.feedback('出现系统异常，请检查日志')
+
+
+class ActionHandler(object):
+    """docstring for ActionHandler"""
+
+    @staticmethod
+    def default(base_handler, result):
+        print 'turing run'
+        robot = TuringRobot(BC.TURING_KEY)
+        try:
+            content = robot.ask_turing(result)
+        except Exception, e:
+            logger.warn(traceback.format_exc())
+            return '没有找到问题的答案'
+        else:
+            return content
+
+    @staticmethod
+    def _memo(date, base_handler):
+        base_handler.feedback('请说出记录内容')
+        audio = base_handler.audio_handler.arecord(6, is_buffer=True)
+        cache_handler = CacheHandler()
+        cache_handler.zset(date, audio, timestamp(), 86400*3)
+        return '完成记录'
+
+    @staticmethod
+    def memo_today(base_handler, result):
+        return ActionHandler._memo(
+            date=datetime.date.today().strftime('%Y-%m-%d'),
+            base_handler=base_handler
+            )
+
+    @staticmethod
+    def memo_tomo(base_handler, result):
+        return ActionHandler._memo(
+            date=(datetime.date.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d'),
+            base_handler=base_handler
+            )
+
+    @staticmethod
+    def play_memo(date, base_handler):
+        cache_handler = CacheHandler()
+        audio = cache_handler.zget(date, 0, -1)
+        if audio:
+            for item in audio:
+                base_handler.audio_handler.aplay(item, is_buffer=True)
+            return '播放结束'
+        else:
+            base_handler.feedback('未找到记录')
+            return None
+
+    @staticmethod
+    def play_memo_tomo(base_handler, result):
+        return ActionHandler.play_memo(
+            date=(datetime.date.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d'),
+            base_handler=base_handler
+            )
+
+    @staticmethod
+    def play_memo_today(base_handler, result):
+        return ActionHandler.play_memo(
+            date=datetime.date.today().strftime('%Y-%m-%d'),
+            base_handler=base_handler
+            )
+
+    @staticmethod
+    def del_memo(date, start, end):
+        cache_handler = CacheHandler()
+        cache_handler.zdel(date, start, end)
+        return '删除成功'
+
+    @staticmethod
+    def del_last_memo(base_handler, result):
+        return ActionHandler.del_memo(
+            date=datetime.date.today().strftime('%Y-%m-%d'),
+            start=-1,
+            end=-1
+            )
+
+    @staticmethod
+    def del_first_memo(base_handler, result):
+        return ActionHandler.del_memo(
+            date=datetime.date.today().strftime('%Y-%m-%d'),
+            start=0,
+            end=0
+            )
+
+    @staticmethod
+    def del_all_memo(base_handler, result):
+        return ActionHandler.del_memo(
+            date=datetime.date.today().strftime('%Y-%m-%d'),
+            start=0,
+            end=-1
+            )
+
+    @staticmethod
+    def weather_tomo(base_handler, result):
+        return ActionHandler.query_weather('tomo')
+
+    @staticmethod
+    def weather_today(base_handler, result):
+        return ActionHandler.query_weather('today')
+
+    @staticmethod
+    def query_weather(today_or_tomo):
+        client = BaiduAPIClient()
+        try:
+            content = client.heweather()
+        except Exception, e:
+            logger.warn(traceback.format_exc())
+            return '查询天气失败，请检查日志'
+        if today_or_tomo == 'today':
+            now_weather = content.get('now')
+            today_weather = content.get('daily_forecast')[0]
+            text = BAC.TODAY_WEATHER_TEXT.format(
+                cond=now_weather['cond']['txt'],
+                fl=now_weather['fl'],
+                hum=now_weather['hum'],
+                min=today_weather['tmp']['min'],
+                max=today_weather['tmp']['max'],
+                txt_d=today_weather['cond']['txt_d'],
+                txt_n=today_weather['cond']['txt_n'],
+                pop=today_weather['pop'],
+                qlty=content['aqi']['city']['aqi']
+                )
+        elif today_or_tomo == 'tomo':
+            tomo_weather = content.get('daily_forecast')[1]
+            text = BAC.TOMO_WEATHER_TEXT.format(
+                min=tomo_weather['tmp']['min'],
+                max=tomo_weather['tmp']['max'],
+                txt_d=tomo_weather['cond']['txt_d'],
+                txt_n=tomo_weather['cond']['txt_n'],
+                pop=tomo_weather['pop']
+                )
+        else:
+            return '查询天气失败，请检查日志'
+        return text
---
+++ raspi/raspi_assistant/utils.py
@@ -0,0 +1,245 @@
+# coding: utf-8
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import time
+import random
+import base64
+from functools import wraps
+from hashlib import md5
+from subprocess import Popen, PIPE
+from tempfile import TemporaryFile
+
+import redis
+import pyaudio
+import wave
+from voicetools import BaseClient, APIError
+
+from .settings import (
+    LogConfig as LC, RedisConfig as RC, BaiduAPIConfig as BAC,
+    BasicConfig as BC, ErrNo)
+
+conn_pool = redis.ConnectionPool(host=RC.HOST_ADDR, port=RC.PORT, db=RC.DB)
+
+
+def convert_to_wav(file_):
+    """convert mp3 to wav"""
+    p = Popen(['ffmpeg', '-y', '-i', '-', '-f', 'wav', BC.OUTPUT_NAME], stdin=file_ , stdout=None, stderr=None)
+    p.wait()
+
+
+def init_logging_handler():
+    handler = TimedRotatingFileHandler(LC.LOGGING_LOCATION, when='MIDNIGHT')
+    formatter = logging.Formatter(LC.LOGGING_FORMAT)
+    handler.setFormatter(formatter)
+    logger = logging.getLogger()
+    logger.setLevel(LC.LOGGING_LEVEL)
+    logger.addHandler(handler)
+    return logger
+
+
+def generate_response():
+    """Generate emotional response based on the HAPPINESS_THRESHOLD."""
+    if random.random() <= BC.HAPPINESS_THRESHOLD:
+        return BC.POSITIVE_ANSWER[random.randint(1, len(BC.POSITIVE_ANSWER))]
+    else:
+        return BC.NEGATIVE_ANSWER[random.randint(1, len(BC.NEGATIVE_ANSWER))]
+
+
+def unique_id(func, args, kwargs):
+    return md5(func.__name__ + repr(args) + repr(kwargs)).hexdigest()
+
+
+def timestamp():
+    return int(time.time() * 1000)
+
+
+def cache(func):
+    """Wrapper for cache the audio"""
+    @wraps(func)
+    def _(*args, **kwargs):
+        cache_handler = CacheHandler()
+        id_ = unique_id(func, *args, **kwargs)
+        cache = cache_handler.get(id_)
+        if cache:
+            audio_handler = AudioHandler()
+            audio_handler.aplay(base64.b64decode(cache), is_buffer=True)
+            # return cache
+        else:
+            func(*args, **kwargs)
+            with open('output.wav', 'rb') as f:
+                encoded_audio = base64.b64encode(f.read())
+                cache_handler.set(id_, encoded_audio, 86400*7)
+            # return buffer_
+    return _
+
+
+class BaiduAPIClient(BaseClient):
+    """Client for handling the process of Baidu APIStore requests."""
+    def __init__(self, **kwargs):
+        super(BaiduAPIClient, self).__init__(**kwargs)
+        self.apikey = BAC.API_KEY
+        self.headers = {'apikey': self.apikey}
+
+    def request_handler(self, url, params):
+        try:
+            resp = self.get_request(
+                url=url,
+                params=params,
+                headers=self.headers)
+        except Exception, e:
+            raise e
+        content = resp.json()
+        if 'errNum' in content:
+            err_msg = 'err_msg: %s' % content.get('errMsg', 'baidu api unknown error')
+            raise ErrNo.ExceptionMap.get(content['errNum'][:4], APIError)(err_msg)
+        else:
+            return content
+
+    def heweather(self):
+        try:
+            content = self.request_handler(
+                url=BAC.WEATHER_URL,
+                params={'city': BC.LOCATION})
+        except Exception, e:
+            raise e
+        weather_info = content['HeWeather data service 3.0'][0]
+        if weather_info['status'] != 'ok':
+            raise APIError('query weather api failed.')
+        return weather_info
+
+
+class AudioHandler(object):
+    """AudioHandler for processing audio, including recording and playing."""
+    def __init__(self, chunk=1024, format_=pyaudio.paInt16, channels=1, rate=16000):
+        self.CHUNK = chunk
+        self.FORMAT = format_
+        self.CHANNELS = channels
+        self.RATE = rate
+
+    def record(self, record_seconds, file_):
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=self.FORMAT,
+                        channels=self.CHANNELS,
+                        rate=self.RATE,
+                        input=True,
+                        input_device_index=0,
+                        frames_per_buffer=self.CHUNK)
+
+        frames = []
+
+        for i in range(0, int(self.RATE / self.CHUNK * record_seconds)):
+            data = stream.read(self.CHUNK)
+            frames.append(data)
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+        wf = wave.open(file_, 'wb')
+        wf.setnchannels(self.CHANNELS)
+        wf.setsampwidth(p.get_sample_size(self.FORMAT))
+        wf.setframerate(self.RATE)
+        wf.writeframes(''.join(frames))
+        wf.close()
+
+    def arecord(self, record_seconds, is_buffer=False, file_=BC.INPUT_NAME):
+        if is_buffer:
+            p = Popen(
+                ['arecord', '-r', '16000', '-D', 'plughw:1,0', '-f', 'S16_LE', '-d', str(record_seconds), '-'],
+                #['arecord', '-r', '16000', '-f', 'S16_LE', '-d', str(record_seconds), '-'],
+                stdout=PIPE,
+                stderr=PIPE)
+            stdout, _ = p.communicate()
+            return stdout
+        else:
+            p = Popen(
+                ['arecord', '-r', '16000', '-D', 'plughw:1,0', '-f', 'S16_LE', '-d', str(record_seconds), file_])
+                #['arecord', '-r', '16000', '-f', 'S16_LE', '-d', str(record_seconds), file_])
+            p.wait()
+
+    def aplay(self, file_=BC.OUTPUT_NAME, is_buffer=False):
+        if is_buffer:
+            temp = TemporaryFile()
+            temp.write(file_)
+            temp.seek(0)
+            p = Popen(['aplay', '-'], stdin=temp)
+            temp.close()
+        else:
+            p = Popen(['aplay', file_])
+        p.wait()
+
+    def play(self, file_):
+        wf = wave.open(file_, 'rb')
+        p = pyaudio.PyAudio()
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+
+        data = wf.readframes(self.CHUNK)
+
+        while data != '':
+            stream.write(data)
+            data = wf.readframes(self.CHUNK)
+
+        stream.stop_stream()
+        stream.close()
+
+        p.terminate()
+
+
+class Keyword(object):
+    """Object for """
+    def __init__(self, list_):
+        list_.sort()
+        self.keywords = list_
+        self.value = '/'.join(list_)
+
+    def __repr__(self):
+        return self.value
+
+
+class CacheHandler(object):
+    """CacheHandler for manipulating redis."""
+    def __init__(self):
+        self.client = redis.StrictRedis(
+            connection_pool=conn_pool, socket_timeout=RC.SOCKET_TIMEOUT)
+
+    def set(self, name, value, ttl=None):
+        if ttl:
+            self.client.setex(name, ttl, value)
+        else:
+            self.client.set(name, value)
+
+    def get(self, name):
+        return self.client.get(name)
+
+    def delete(self, name):
+        return self.client.delete(name)
+
+    def expire(self, name, ttl):
+        return self.client.expire(name, ttl)
+
+    def zset(self, name, key, score, ttl=None, is_audio=True):
+        """zset for audio. if is_audio=True, """
+        if is_audio:
+            key = base64.b64encode(key)
+        if ttl:
+            pipeline = self.client.pipeline()
+            pipeline.zadd(name, score, key)
+            pipeline.expire(name, ttl)
+            return pipeline.execute()
+        else:
+            self.client.zadd(name, score, key)
+
+    def zget(self, name, start, end, is_audio=True):
+        ret = self.client.zrange(name, start, end)
+        if is_audio:
+            return [base64.b64decode(x) for x in ret]
+        else:
+            return ret
+
+    def zdel(self, name, start, end):
+        # zremrangebyrank
+        return self.client.zremrangebyrank(name, start, end)
